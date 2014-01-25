59a85ea280868d51f3fc732bdaaee5cb74632a3b
David Carr <dcarr@scienceprousa.com>
2014-01-25 02:30:52 -0800

    something something

--- Android/app/src/main/java/com/suchroadtrip/app/activities/MainActivity.java
+++ Android/app/src/main/java/com/suchroadtrip/app/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
