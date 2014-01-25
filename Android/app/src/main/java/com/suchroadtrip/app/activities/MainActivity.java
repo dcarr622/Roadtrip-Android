c71faf7059c514779a1e2b3b9805d525132d2a0f
David Carr <dcarr@scienceprousa.com>
2014-01-24 23:31:18 -0800

    start map fragment

--- Android/app/src/main/java/com/suchroadtrip/app/activities/MainActivity.java
+++ Android/app/src/main/java/com/suchroadtrip/app/activities/MainActivity.java
@@ -1,22 +1,21 @@
-package com.suchroadtrip.app;
+package com.suchroadtrip.app.activities;
 
 import java.util.Locale;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.suchroadtrip.app.R;
 
 public class MainActivity extends Activity {
 
