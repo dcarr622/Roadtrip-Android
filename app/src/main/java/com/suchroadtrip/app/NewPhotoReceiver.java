9e9064d536c3526b55c7304f25e9f96f10053e5e
Vinnie Magro <vmagro@socaldevs.com>
2014-01-25 10:42:14 -0800

    Juicy auto-refresh when taking a picture. A bunch of other stuff too

--- app/src/main/java/com/suchroadtrip/app/NewPhotoReceiver.java
+++ app/src/main/java/com/suchroadtrip/app/NewPhotoReceiver.java
@@ -3,7 +3,12 @@ package com.suchroadtrip.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.util.Log;
+
+import com.suchroadtrip.lib.RTApi;
 
 /**
  * Created by vmagro on 1/25/14.
@@ -13,6 +18,12 @@ public class NewPhotoReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("newphoto", intent.getData().toString());
+        LocationManager mgr = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = mgr.getBestProvider(criteria, true);
+        Location loc = mgr.getLastKnownLocation(provider);
+        RTApi.addPicture(context, intent.getData(), loc);
     }
 
 }
--- app/src/main/java/com/suchroadtrip/app/data/TripEventAdapter.java
+++ app/src/main/java/com/suchroadtrip/app/data/TripEventAdapter.java
@@ -2,12 +2,17 @@ package com.suchroadtrip.app.data;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+import com.suchroadtrip.app.R;
 import com.suchroadtrip.lib.RTOpenHelper;
 
 /**
@@ -17,6 +22,8 @@ public class TripEventAdapter extends CursorAdapter {
 
     private static final String TAG = "TripEventAdapter";
 
+    private static final String FORMAT_MAP_URL = "http://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=13&size=100x100&key=AIzaSyCAJbA5io-Jb97Y-vwSNJI6y64DcCyAZoE&sensor=false";
+
     public TripEventAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
         Log.d(TAG, "loaded " + cursor.getCount() + " events");
@@ -24,21 +31,89 @@ public class TripEventAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        TextView tv = new TextView(context);
-        tv.setText("blahblahblah");
-
+        View v = null;
         if (cursor.getColumnIndex(RTOpenHelper.KEY_SOCIAL_SERVICE) != -1) {
+            v = LayoutInflater.from(context).inflate(R.layout.feed_social, null);
+            v.setTag(new EventTag(EventTag.Type.SOCIAL, v));
             Log.d(TAG, "social event");
         } else if (cursor.getColumnIndex(RTOpenHelper.KEY_PHOTO_URI) != -1) {
+            v = LayoutInflater.from(context).inflate(R.layout.feed_media, null);
+            v.setTag(new EventTag(EventTag.Type.PHOTO, v));
             Log.d(TAG, "photo event");
         } else if (cursor.getColumnIndex(RTOpenHelper.KEY_NAME) != -1) {
+            v = LayoutInflater.from(context).inflate(R.layout.feed_stop, null);
+            v.setTag(new EventTag(EventTag.Type.LOCATION, v));
             Log.d(TAG, "location event");
         }
-        return tv;
+        return v;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, Cursor c) {
+        if (view.getTag() != null && view.getTag() instanceof EventTag) {
+            EventTag tag = (EventTag) view.getTag();
+
+            Picasso picasso = Picasso.with(context);
+
+            if (tag.map != null){
+                String url = String.format(FORMAT_MAP_URL, c.getDouble(c.getColumnIndex(RTOpenHelper.KEY_LAT)), c.getDouble(c.getColumnIndex(RTOpenHelper.KEY_LNG)));
+                Log.d(TAG, url);
+                picasso.load(url).into(tag.map);
+            }
+
+            switch (tag.getType()) {
+                case SOCIAL:
+                    tag.postText.setText(c.getString(c.getColumnIndex(RTOpenHelper.KEY_TEXT)));
+                    tag.authorText.setText(c.getString(c.getColumnIndex(RTOpenHelper.KEY_POSTER)));
+                    tag.networkText.setText("via " + c.getString(c.getColumnIndex(RTOpenHelper.KEY_SOCIAL_SERVICE)));
+                    break;
+                case PHOTO:
+                    picasso.load(Uri.parse(c.getString(c.getColumnIndex(RTOpenHelper.KEY_PHOTO_URI)))).resize(100, 100).into(tag.mediaImage);
+                    break;
+            }
+        }
+    }
+
+    private static class EventTag {
+        public static enum Type {
+            SOCIAL, PHOTO, LOCATION
+        }
+
+        private Type type;
+
+        public ImageView map;
+
+        public TextView postText;
+        public TextView networkText;
+        public TextView authorText;
+
+        public TextView mediaText;
+        public ImageView mediaImage;
+
+        public EventTag(Type type, View v) {
+            this.type = type;
+
+            map = (ImageView) v.findViewById(R.id.mapImage);
+
+            switch (type) {
+                case SOCIAL:
+                    postText = (TextView) v.findViewById(R.id.postText);
+                    networkText = (TextView) v.findViewById(R.id.networkText);
+                    authorText = (TextView) v.findViewById(R.id.authorText);
+                    break;
+                case PHOTO:
+                    authorText = (TextView) v.findViewById(R.id.authorText);
+                    mediaText = (TextView) v.findViewById(R.id.mediaText);
+                    mediaImage = (ImageView) v.findViewById(R.id.mediaImage);
+                    break;
+            }
+
+        }
+
+        public Type getType() {
+            return type;
+        }
 
     }
+
 }
