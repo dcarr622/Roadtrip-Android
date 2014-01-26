45f461b0298a93b6fe42821940691abc68cc9f67
Ian Whelan <ijwhelan@gmail.com>
2014-01-26 03:07:37 -0800

    new icon

--- app/src/main/java/com/suchroadtrip/app/fragments/RoadtripMapFragment.java
+++ app/src/main/java/com/suchroadtrip/app/fragments/RoadtripMapFragment.java
@@ -2,6 +2,7 @@ package com.suchroadtrip.app.fragments;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
