f99a281ffa270a5f01e4f12b481278e8e3eef897
Vinnie Magro <vmagro@socaldevs.com>
2014-01-26 01:34:04 -0800

    Switch back to static maps

--- lib/src/main/java/com/suchroadtrip/lib/Util.java
+++ lib/src/main/java/com/suchroadtrip/lib/Util.java
@@ -7,7 +7,7 @@ import android.location.Location;
  */
 public class Util {
 
-    private static final String FORMAT_MAP_URL = "http://maps.googleapis.com/maps/api/staticmap?center=%f,%f&markers=%f,%f&zoom=13&size=200x200&key=AIzaSyCAJbA5io-Jb97Y-vwSNJI6y64DcCyAZoE&sensor=false";
+    private static final String FORMAT_MAP_URL = "http://maps.googleapis.com/maps/api/staticmap?center=%f,%f&markers=%f,%f&zoom=13&size=400x400&key=AIzaSyCAJbA5io-Jb97Y-vwSNJI6y64DcCyAZoE&sensor=false";
 
     public static String buildMapUrl(Location loc) {
         String url = String.format(FORMAT_MAP_URL, loc.getLatitude(), loc.getLongitude(), loc.getLatitude(), loc.getLongitude());
